dbc4f8c487d1e64f252752c866205bc59e825ca3
BlexxeCode <102894148+BlexxeCode@users.noreply.github.com>
2023-06-07 22:57:09 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import praw
 
 reddit = praw.Reddit(
-    client_id="8pkUcqC1Q3K8E0GWSzxElg",
-    client_secret="MUImaZdXqoX7G4uPacBqVyyMm054GQ",
-    password="aanu4321",
+    client_id="",
+    client_secret="",
+    password="",
     user_agent="the 'Soon To Be' best",
-    username="Maleexe",
+    username="",
 )
